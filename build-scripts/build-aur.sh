8a9d7b3f8318416a12074a84ab484c7298ca2e24
PierpaoloSpadafora <pierpaolospadafora23+github@gmail.com>
2025-08-06 13:30:28 +0200

    quick push

--- build-scripts/build-aur.sh
+++ build-scripts/build-aur.sh
@@ -25,28 +25,35 @@ echo "Installing build dependencies..."
 sudo pacman -S --needed python python-build python-installer python-wheel python-setuptools
 
 echo "Preparing source package..."
-rm -rf src pkg
+rm -rf src pkg *.pkg.tar.*
 mkdir -p src
 
-echo "Creating clean source copy..."
-rsync -av \
-    --exclude='.git/' \
-    --exclude='dist/' \
-    --exclude='build/' \
-    --exclude='src/' \
-    --exclude='pkg/' \
-    --exclude='*.egg-info/' \
-    --exclude='__pycache__/' \
+echo "Creating source tarball..."
+# Crea il tarball nella directory corrente per makepkg
+cd ..
+tar --exclude='.git' \
+    --exclude='dist' \
+    --exclude='build' \
+    --exclude='src' \
+    --exclude='pkg' \
+    --exclude='*.egg-info' \
+    --exclude='__pycache__' \
     --exclude='*.pyc' \
-    --exclude='.venv*/' \
-    --exclude='venv/' \
-    --exclude='env/' \
+    --exclude='.venv*' \
+    --exclude='venv' \
+    --exclude='env' \
     --exclude='*.pkg.tar.*' \
-    --exclude='.idea/' \
-    ../ src/TextMerger-"$VERSION"/
+    --exclude='.idea' \
+    --exclude='.flatpak-builder' \
+    --exclude='flatpak-build' \
+    -czf "build-scripts/TextMerger-$VERSION.tar.gz" \
+    --transform="s,^,TextMerger-$VERSION/," \
+    --exclude='build-scripts/src' \
+    --exclude='build-scripts/pkg' \
+    --exclude='build-scripts/TextMerger' \
+    .
 
-cp ../packaging/*       src/TextMerger-"$VERSION"/
-cp ../docs/LICENSE      src/TextMerger-"$VERSION"/
+cd build-scripts
 
 echo "Building Arch package..."
 makepkg -sf
